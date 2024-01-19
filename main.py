7849ca040810baa2972c3125b16193f001ee3a98
Andrei Kalinichenko <muse2140@gmail.com>
2024-01-19 22:28:05 +0200

    Task 3

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# 1.
-# with open('file.txt', 'r', encoding='utf-8') as input_file:
-#     content = input_file.read()
-#     words = content.split()
-#
-#     filtered_words = [word for word in words if len(word) >= 7]
-#
-# with open('file.txt', 'w', encoding='utf-8') as output_file:
-#     output_file.write(' '.join(filtered_words))
-#
-# print(filtered_words)
-
-# 2
-# with open('file.txt', 'r', encoding='utf-8') as file:
-#     content = file.read()
-#     words = content.split()
-#     word_count = len(words)
-#
-# print(f'The number of words in the file: {word_count}')
-
 # 3
 # def check_and_replace(file_path, forbidden_word):
 #     with open(file_path, 'r', encoding='utf-8') as file:
